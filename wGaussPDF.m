a5da26ed1910ad6a0e9f90594e35be7b9b73a3fb
cr05512 <alessandrodortenzio@hotmail.it>
2020-04-09 19:19:45 +0200

    Renamed gauss component class

---
+++ wGaussPDF.m
@@ -0,0 +1,52 @@
+classdef wGaussPDF < dynamicprops
+%Weighted gaussian PDFs
+    properties
+        w
+        mu
+        Sigma
+    end
+    
+    methods
+        function obj = wGaussPDF(weight,mean,covariance)
+            if nargin > 0
+                obj.w = weight;
+                obj.mu = mean;
+                obj.Sigma = covariance;
+            end
+        end
+        
+        function weight = getWeight(obj)
+            weight = obj.w;
+        end
+        
+        function mean = getMean(obj)
+            mean = obj.mu;
+        end
+        
+        function covariance = getCovariance(obj)
+            covariance = obj.Sigma;
+        end
+        
+        function setWeight(obj, weight)
+            obj.w = weight;
+        end
+        
+        function setMean(obj, mean)
+            obj.mu = mean;
+        end
+        
+        function setCovariance(obj, covariance)
+            obj.Sigma = covariance;
+        end
+        
+        function compPDFValues = getPDFValues(obj,X)
+            if size(obj.Sigma,1)>1
+                compPDFValues = mvnpdf(X,obj.mu',obj.Sigma);
+            else
+                compPDFValues = pdf('Normal',X,obj.mu,obj.Sigma);
+            end
+        end
+    
+    end
+end
+
