5453ca323e4358aef77d994ccdce5d96c8abc0a2
cr05512 <alessandrodortenzio@hotmail.it>
2020-06-08 23:54:54 +0200

    Some clean up

--- GMRCMod.m
+++
@@ -1,9 +0,0 @@
-function [gmr,nISETrajGMRC] = GMRCMod(gmh,Nr,NKMeansSteps,sk,NOptSteps,optWeights)
-%This function implements the Gaussian Mixture Reduction via Clustering
-%Algorithm
-gmr = RunnalsMRA(gmh,Nr);
-gm_KM = KMeansnISE(gmh,gmr,NKMeansSteps);
-[gmr, nISETrajGMRC] = ISEOpt(gmh,gm_KM,sk,NOptSteps,optWeights);
-
-end
-
