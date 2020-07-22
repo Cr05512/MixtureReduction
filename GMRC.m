15ef0c81af807c63b031f01de7cffff257e4fcd0
cr05512 <alessandrodortenzio@hotmail.it>
2020-07-22 19:20:10 +0200

    Minor Fixes

--- GMRC.m
+++ GMRC.m
@@ -3,7 +3,7 @@ function [gmr,nISETrajGMRC] = GMRC(gmh,Nr,NKMeansSteps,sk,NOptSteps,optWeights)
 %Algorithm
 gmr = RunnalsMRA(gmh,Nr);
 gm_KM = KMeansKLD(gmh,gmr,NKMeansSteps);
-[gmr,nISETrajGMRC] = ISEOpt(gmh,gmr,sk,NOptSteps,optWeights);
+[gmr,nISETrajGMRC] = ISEOpt(gmh,gm_KM,sk,NOptSteps,optWeights);
 
 end
 
