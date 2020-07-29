a351adbd9a8fac51b98c49d3cbcda368960dce65
cr05512 <alessandrodortenzio@hotmail.it>
2020-07-29 17:16:08 +0200

    Generalized the CTD calculation to arbitrary cost function. At this point W2 and KLD are available

--- CTDW2.m
+++ CTDW2.m
@@ -1,12 +1,24 @@
-function [d,C,pi_star] = CTDW2(gmh,gmr)
+function [d,C,pi_star] = CTDW2(gmh,gmr,cost_meas)
 %This function computes the composite transportation distance between two gaussian mixtures 
 Nh = length(gmh);
 Nr = length(gmr);
 C = zeros(Nh,Nr);
-for i=1:length(gmh)
-    for j=1:length(gmr)
-        C(i,j) = Wasserstein2Dist(gmh(i),gmr(j));
+
+if strcmp(cost_meas,'KLD')
+    for i=1:length(gmh)
+        for j=1:length(gmr)
+            C(i,j) = KLD(gmh(i),gmr(j));
+        end
     end
+elseif strcmp(cost_meas,'W2')
+    for i=1:length(gmh)
+        for j=1:length(gmr)
+            C(i,j) = Wasserstein2Dist(gmh(i),gmr(j));
+        end
+    end
+else
+    disp('Unknown cost function, aborting...');
+    C = Inf(Nh,Nr);
 end
 
 pi_star = computeOTP(gmh,gmr);
--- ERCTDW2.m
+++ ERCTDW2.m
@@ -1,13 +1,26 @@
-function [d,C,pi_star] = ERCTDW2(gmh,gmr,gamma,maxiter)
+function [d,C,pi_star] = ERCTDW2(gmh,gmr,cost_meas,gamma,maxiter)
 %This function computes the entropic regularized composite transportation distance between two gaussian mixtures 
 Nh = length(gmh);
 Nr = length(gmr);
 C = zeros(Nh,Nr);
-for i=1:length(gmh)
-    for j=1:length(gmr)
-        C(i,j) = Wasserstein2Dist(gmh(i),gmr(j));
+
+if strcmp(cost_meas,'KLD')
+    for i=1:length(gmh)
+        for j=1:length(gmr)
+            C(i,j) = KLD(gmh(i),gmr(j));
+        end
     end
+elseif strcmp(cost_meas,'W2')
+    for i=1:length(gmh)
+        for j=1:length(gmr)
+            C(i,j) = Wasserstein2Dist(gmh(i),gmr(j));
+        end
+    end
+else
+    disp('Unknown cost function, aborting...');
+    C = Inf(Nh,Nr);
 end
+
 
 pi_star = computeEROTP(gmh,gmr,gamma,maxiter);
 
